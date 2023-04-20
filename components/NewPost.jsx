b320387114cf1f7954eee2fcad891f75f55c9956
Sumanth <hellosumax@gmail.com>
2023-04-20 23:16:29 +0530

    added form submission

--- components/NewPost.jsx
+++ components/NewPost.jsx
@@ -1,17 +1,58 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css"
 
 
-function NewPost(props) {
+function NewPost({onCancel}) {
+    // enteredBody & enteredAuthor are state variable(s)
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');        
+
+    // functions that get triggered onChange in the form component
+    // 'event' object parameter for event handler fn
+    // body + ChangeHandler
+    function bodyChangeHandler(event){
+        setEnteredBody(event.target.value)
+    }
+
+    // author + ChangeHandler
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value)
+    }
+    
+    // to submit form data
+    function submitHandler(event) {
+        // prevents request to server
+        event.preventDefault();
+        const postData = {
+            body: enteredBody,
+            author: enteredAuthor
+        };
+        console.log(postData);
+        // hideModalHandler() on submitting
+        onCancel();
+            
+    }
+    
     return (
-        <form className={classes.form}>
+        // onSubmit is triggered when form submit event occurs, sends server request
+        <form className={classes.form} onSubmit={submitHandler}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={props.onBodyChange}></textarea>
+                {/* browser form validation using 'required' */}
+                <textarea id="body" required rows={3} onChange={bodyChangeHandler}></textarea>
             </p>            
 
             <p>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name" required onChange={props.onAuthorChange}/>
+                <input type="text" id="name" required onChange={authorChangeHandler}/>
+            </p>
+
+            {/* jsx:className::html:class */}
+            {/* classes.actions = '.actions' */}
+            <p className={classes.actions}>
+                <button type="button" onClick={onCancel}>Cancel</button>
+                {/* default button type is 'submit' */}
+                <button type="submit">Submit</button>
             </p>
         </form>
     );
--- components/PostsList.jsx
+++ components/PostsList.jsx
@@ -2,44 +2,23 @@ import classes from "./PostsList.module.css"
 import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
-import { useState } from "react";
 
 // destructuring assignment of obj: const {isPosting, onStopPosting} = props; 
 function PostsList({isPosting, onStopPosting}) {
-    // 'state' is lifted up to be used in another sibling Post    
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-    
-
-    // functions that get triggered onChange in the form component
-    // body + ChangeHandler
-    function bodyChangeHandler(event){
-        setEnteredBody(event.target.value)
-    }
-
-    // author + ChangeHandler
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value)
-    }    
-
     return (
         <>
             {/* Modal component is an overlay of the NewPost compt */}
             {/* passing fnName: fn itself vs fnName(): return value of fn */}
-            {/* 'Modal' is displayed if state is truthy */}
-            {console.log('isPosting', isPosting)}            
+            {/* 'Modal' is displayed if state is truthy */}           
             {isPosting && (
                 // conditional content to be rendered                
                 <Modal onClose={onStopPosting}>
                 <NewPost  
-                    onBodyChange={bodyChangeHandler} 
-                    onAuthorChange={authorChangeHandler}>
+                    onCancel={onStopPosting}>
                 </NewPost>
                 </Modal>
             )}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} book={enteredBody}></Post>
-                <Post author="Jeffrey Archer" book="Kane & Abel"/>
             </ul>
         
         </>
